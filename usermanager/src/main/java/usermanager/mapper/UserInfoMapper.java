9442e08f6dae96bf694b352ecd4fe37203aef7c5
dongming <350376345@qq.com>
2018-01-07 13:01:55 +0800

    添加查询用户信息的mapper。

--- usermanager/src/main/java/usermanager/mapper/UserInfoMapper.java
+++ usermanager/src/main/java/usermanager/mapper/UserInfoMapper.java
@@ -1,8 +1,27 @@
-        package usermanager.mapper;
+package usermanager.mapper;
 
-        public interface UserInfoMapper
+import org.apache.ibatis.annotations.Select;
+
+import usermanager.entity.UserBaseInfo;
+
+/**
+ * 用户信息的
+ * 
+ * @Title:
+ * @Description:
+ * @Author:chenmin
+ * @Since:2018年1月7日
+ * @Version:1.1.0
+ */
+public interface UserInfoMapper
 {
-    
+    /**
+     * 根据用户名查询用户的基本信息
+     * 
+     * @param Name
+     * @return
+     * @Description:
+     */
+    @Select("SELECT userId,username,pwd FROM tbl_users")
+    UserBaseInfo getUserBaseInfoByName(String Name);
 }
-
-    
