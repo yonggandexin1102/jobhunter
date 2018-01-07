46ed4197ff8d25fa7697859cc7709732b8b932f5
dongming <350376345@qq.com>
2018-01-07 12:02:38 +0800

    相关配置文件和目录上库。

--- usermanager/src/main/java/usermanager/entity/UserBaseInfo.java
+++ usermanager/src/main/java/usermanager/entity/UserBaseInfo.java
@@ -1,5 +1,59 @@
 package usermanager.entity;
 
-public class UserBaseInfo {
-
+/**
+ * 用户基本信息
+ * 
+ * @Title:
+ * @Description:
+ * @Author:chenmin
+ * @Since:2018年1月7日
+ * @Version:1.1.0
+ */
+public class UserBaseInfo
+{
+    /**
+     * 用户名，本网站中唯一。
+     */
+    private String userName;
+    
+    /**
+     * 用户id，用32位UUID标识
+     */
+    private String userId;
+    
+    /**
+     * 用户密码
+     */
+    private String pwd;
+    
+    public String getUserName()
+    {
+        return userName;
+    }
+    
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+    
+    public String getUserId()
+    {
+        return userId;
+    }
+    
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+    
+    public String getPwd()
+    {
+        return pwd;
+    }
+    
+    public void setPwd(String pwd)
+    {
+        this.pwd = pwd;
+    }
+    
 }
